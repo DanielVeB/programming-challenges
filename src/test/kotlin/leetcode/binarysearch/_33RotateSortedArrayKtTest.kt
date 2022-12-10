4bcf48b17a7ce684054272bc0b4927b43b990c90
Daniel Kurosz <danielkurosz@gmail.com>
2022-12-10 23:54:01 +0100

    uncomment test cases

--- src/test/kotlin/leetcode/binarysearch/_33RotateSortedArrayKtTest.kt
+++ src/test/kotlin/leetcode/binarysearch/_33RotateSortedArrayKtTest.kt
@@ -10,27 +10,26 @@ internal class _33RotateSortedArrayKtTest {
 
     companion object {
         @JvmStatic
-        fun generateData(): Stream<Arguments> =
-            Stream.of(
-//                Arguments.of(
-//                    intArrayOf(4, 5, 6, 7, 0, 1, 2), 0, 4
-//                ),
-//                Arguments.of(
-//                    intArrayOf(4, 5, 6, 7, 0, 1, 2), 3, -1
-//                ),
-//                Arguments.of(
-//                    intArrayOf(1, 3), 2, -1
-//                ),
-//                Arguments.of(
-//                    intArrayOf(8, 9, 2, 3, 4), 9, 1
-//                ),
-//                Arguments.of(
-//                    intArrayOf(4, 5, 6, 7, 0, 1, 2), 5, 1
-//                ),
-                Arguments.of(
-                    intArrayOf(7,8,1,2,3,4,5,6), 2, 3
-                ),
-                )
+        fun generateData(): Stream<Arguments> = Stream.of(
+            Arguments.of(
+                intArrayOf(4, 5, 6, 7, 0, 1, 2), 0, 4
+            ),
+            Arguments.of(
+                intArrayOf(4, 5, 6, 7, 0, 1, 2), 3, -1
+            ),
+            Arguments.of(
+                intArrayOf(1, 3), 2, -1
+            ),
+            Arguments.of(
+                intArrayOf(8, 9, 2, 3, 4), 9, 1
+            ),
+            Arguments.of(
+                intArrayOf(4, 5, 6, 7, 0, 1, 2), 5, 1
+            ),
+            Arguments.of(
+                intArrayOf(7, 8, 1, 2, 3, 4, 5, 6), 2, 3
+            ),
+        )
     }
 
 
